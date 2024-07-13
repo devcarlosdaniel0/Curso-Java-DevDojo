fc7d7ae2c0933c615852d8094b32189dc5904729
Carlos <cadanielmarchesin@gmail.com>
2024-07-13 10:20:42 -0300

    Aula pt2 coesão

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/ProfessorTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Professor;
+
+public class ProfessorTest01 {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+    }
+}
