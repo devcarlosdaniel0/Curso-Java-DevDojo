62bdabbe7cba2fe7f9f9971775c0383318ca400a
Carlos <cadanielmarchesin@gmail.com>
2024-07-14 11:09:46 -0300

    return

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -15,4 +15,11 @@ public class Calculadora {
         int multiplicao = a * b;
         System.out.println(multiplicao);
     }
+
+    public double divideDoisNumeros (double a, double b) {
+        if (b == 0) {
+            return 0;
+        }
+        return a/b;
+    }
 }
