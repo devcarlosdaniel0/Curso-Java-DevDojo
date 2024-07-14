d88bcebbf3a813c0d9594f20f750fb3a010674b8
Carlos <cadanielmarchesin@gmail.com>
2024-07-14 10:59:33 -0300

    métodos

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+    public void somaDoisNumeros(int a, int b) {
+        int soma = a + b;
+        System.out.println(soma);
+    }
+
+    public void subtracaoDoisNumeros(int a, int b) {
+        int subtracao = a - b;
+        System.out.println(subtracao);
+    }
+
+    public void multiplicaDoisNumeros(int a, int b) {
+        int multiplicao = a * b;
+        System.out.println(multiplicao);
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros(5,20);
+        calculadora.subtracaoDoisNumeros(20,10);
+        calculadora.multiplicaDoisNumeros(5,9);
+    }
+}
+ 
