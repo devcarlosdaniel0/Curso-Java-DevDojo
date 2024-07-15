8a900187d800ba804d6c1afbe24a78e973cd54b0
Carlos <cadanielmarchesin@gmail.com>
2024-07-15 11:29:15 -0300

    Construtores e sobrecarga

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.test;
+
+import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Hajime no Ippo", "TV", 300, "luta", "KONAMI");
+        anime.imprime();
+    }
+}
