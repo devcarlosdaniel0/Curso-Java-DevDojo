eca6605e1d204a9c1d64e60885346a849ead6f50
Carlos <cadanielmarchesin@gmail.com>
2024-07-16 10:12:46 -0300

    super

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Gheranca/dominio/Endereco.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Gheranca.dominio;
+
+public class Endereco {
+    private String rua, cep;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Gheranca/test/HerancaTest01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Gheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Gheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Gheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Gheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 13° de Maio");
+        endereco.setCep("13421-043");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Richard");
+        pessoa.setCpf("259.432.432-66");
+        pessoa.setEndereco(endereco);
+
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Kayn");
+        funcionario.setCpf("492.329.342-66");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(2500.50f);
+        System.out.println("-------------------------");
+        funcionario.imprime();
+    }
+}
