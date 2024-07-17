4abbfe89805608e1ac2c1ed62e26c42fcbacd137
Carlos <cadanielmarchesin@gmail.com>
2024-07-17 11:49:33 -0300

    Implementando múltiplas interfaces

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Kinterfaces/test/DataLoaderTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Kinterfaces.test;
+
+import academy.devdojo.maratonajava.javacore.Kinterfaces.dominio.DataBaseLoader;
+import academy.devdojo.maratonajava.javacore.Kinterfaces.dominio.FileLoader;
+
+public class DataLoaderTest01 {
+    public static void main(String[] args) {
+        DataBaseLoader dataBaseLoader = new DataBaseLoader();
+        FileLoader fileLoader = new FileLoader();
+
+        dataBaseLoader.load();
+        fileLoader.load();
+
+        dataBaseLoader.remove();
+        fileLoader.remove();
+
+        dataBaseLoader.checkPermission();
+        fileLoader.checkPermission();
+    }
+}
