b7efc1facc495e8c83f7dba7af9b46f5a0e01ede
Carlos <cadanielmarchesin@gmail.com>
2024-07-17 11:18:12 -0300

    Métodos abstratos

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Jclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Jclassesabstratas.test;
+
+import academy.devdojo.maratonajava.javacore.Jclassesabstratas.dominio.Desenvolvedor;
+import academy.devdojo.maratonajava.javacore.Jclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("António", 3500);
+        System.out.println(gerente);
+        Desenvolvedor dev = new Desenvolvedor("Akita",14000);
+        System.out.println(dev);
+    }
+}
