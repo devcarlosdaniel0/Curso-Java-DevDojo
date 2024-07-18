768c04fd9759746bd46a42fcc0ae2a71ebc677cd
Carlos <cadanielmarchesin@gmail.com>
2024-07-18 16:28:05 -0300

    stackoverflow e runtime excpetion

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Mexception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.Mexception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2};
+        System.out.println(nums[10]);
+    }
+}
