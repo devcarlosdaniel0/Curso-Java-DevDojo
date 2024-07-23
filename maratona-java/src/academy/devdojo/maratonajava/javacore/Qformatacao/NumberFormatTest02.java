b8a0a105c8b1b1e8f7a54667f7574f8aeb28cf62
Carlos <cadanielmarchesin@gmail.com>
2024-07-23 13:26:55 -0300

    Internacionalização de moeda com Locale

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Qformatacao/NumberFormatTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Qformatacao/NumberFormatTest02.java
@@ -4,18 +4,18 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
         Locale localeBR = new Locale("pt","BR");
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
         NumberFormat[] nfa = new NumberFormat[4];
-        nfa[0] = NumberFormat.getInstance();
-        nfa[1] = NumberFormat.getInstance(localeJP);
-        nfa[2] = NumberFormat.getInstance(localeIT);
-        nfa[3] = NumberFormat.getInstance(localeBR);
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeIT);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeBR);
 
-        double valor = 1_000.214;
+        double valor = 1_000_000.214;
 
         for (NumberFormat numberFormat : nfa) {
             numberFormat.setMaximumFractionDigits(2);
@@ -23,7 +23,7 @@ public class NumberFormatTest01 {
             System.out.println(numberFormat.format(valor));
         }
 
-        String valorString = "1000.9613";
+        String valorString = "£1000";
         try {
             System.out.println(nfa[0].parse(valorString));
         } catch (ParseException e) {
