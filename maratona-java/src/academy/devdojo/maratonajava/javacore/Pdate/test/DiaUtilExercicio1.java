e117c1f3d686b4374c48d780dda49df28c66c0bf
Carlos <cadanielmarchesin@gmail.com>
2024-07-24 21:29:38 -0300

    Outra maneira de fazer para calcular o dia útil

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Pdate/test/DiaUtilExercicio1.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Pdate/test/DiaUtilExercicio1.java
@@ -3,15 +3,13 @@ package academy.devdojo.maratonajava.javacore.Pdate.test;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
-public class DiaUtilExercicio {
+public class DiaUtilExercicio1 {
     public static void main(String[] args) {
         // Dado uma determinada data, retorne o próximo dia útil dessa data
 
-        // Data especificada
         LocalDate data = LocalDate.of(2024, 7, 26);
         System.out.println("Hoje é " + data.getDayOfWeek());
 
-        // Chamada do método para obter o próximo dia útil
         LocalDate proximoDiaUtil = obterProximoDiaUtil(data);
         System.out.println("O próximo dia útil é " + proximoDiaUtil.getDayOfWeek() + " (" + proximoDiaUtil + ")");
     }
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Pdate/test/DiaUtilExercicio2.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.Pdate.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjusters;
+
+public class DiaUtilExercicio2 {
+    public static void main(String[] args) {
+        // Dado uma determinada data, retorne o próximo dia útil dessa data
+        LocalDate data = LocalDate.of(2024,07,28);
+        System.out.println("Hoje é " + data.getDayOfWeek());
+        System.out.println("Dia " + data);
+
+        System.out.println("-------------------------");
+
+        LocalDate proximoDiaUtil = obterProximoDiaUtil(data);
+        System.out.println("O próximo dia útil é " + proximoDiaUtil.getDayOfWeek());
+        System.out.println(proximoDiaUtil);
+    }
+
+    public static LocalDate obterProximoDiaUtil(LocalDate data) {
+        DayOfWeek dayOfWeek = data.getDayOfWeek();
+        LocalDate proximoDiaUtil;
+        if (dayOfWeek.equals(DayOfWeek.FRIDAY) || dayOfWeek.equals(DayOfWeek.SATURDAY)) {
+            proximoDiaUtil = data.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+        } else {
+            proximoDiaUtil = data.plusDays(1);
+        }
+        return proximoDiaUtil;
+    }
+}
