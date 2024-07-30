d8b2924425a7d0ac1aafd1f2c672507188fa8d4c
Carlos <cadanielmarchesin@gmail.com>
2024-07-30 19:02:55 -0300

    SimpleFileVisitor pt 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Unio/test/SimpleFileVisitorTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Unio/test/SimpleFileVisitorTest02.java
@@ -4,19 +4,17 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
-class ListJavaFiles extends SimpleFileVisitor<Path> {
+class ListAllFiles extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-        if (file.getFileName().toString().endsWith(".java")) {
-            System.out.println(file.getFileName());
-        }
+        System.out.println(file.getFileName());
         return FileVisitResult.CONTINUE;
     }
 }
 
-public class SimpleFileVisitorTest01 {
+public class SimpleFileVisitorTest02 {
     public static void main(String[] args) throws IOException {
         Path root = Paths.get(".");
-        Files.walkFileTree(root, new ListJavaFiles());
+        Files.walkFileTree(root, new ListAllFiles());
     }
 }
