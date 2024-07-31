729d0e68075d98ebae299cb243c0962c175e55c5
Carlos <cadanielmarchesin@gmail.com>
2024-07-31 14:23:53 -0300

    equals pt 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Wcolecoes/test/EqualsTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wcolecoes/test/EqualsTest01.java
@@ -8,6 +8,5 @@ public class EqualsTest01 {
         Smartphone s2 = new Smartphone("1ABC1", "Samsung");
 
         System.out.println(s1.equals(s2));
-        System.out.println(s1.getSerialNumber().equals(s2.getSerialNumber()));
     }
 }
