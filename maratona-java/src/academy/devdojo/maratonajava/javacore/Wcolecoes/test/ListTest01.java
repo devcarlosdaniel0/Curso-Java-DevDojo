0b8bf922114dfaf34e3b4aed45bfe54142bc5c3c
Carlos <cadanielmarchesin@gmail.com>
2024-07-31 16:39:32 -0300

    List pt 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Wcolecoes/test/ListTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wcolecoes/test/ListTest01.java
@@ -5,19 +5,27 @@ import java.util.List;
 
 public class ListTest01 {
     public static void main(String[] args) {
-        List<String> carros = new ArrayList<>();
-        carros.add("Mercedez");
-        carros.add("Volkswagen");
+        List<String> nomes1 = new ArrayList<>();
+        List<String> nomes2 = new ArrayList<>();
+        nomes1.add("Carlos");
+        nomes1.add("Daniel");
+        nomes2.add("Marchesin");
+        nomes2.add("da Silva");
+        
+        nomes1.addAll(nomes2);
 
-        for (String carro : carros) {
-            System.out.println(carro);
+        for (String nome : nomes1) {
+            System.out.println(nome);
         }
 
         System.out.println("--------------");
-        carros.add("Lamborghini");
 
-        for (int i = 0; i < carros.size(); i++) {
-            System.out.println(carros.get(i));
+        for (int i = 0; i < nomes1.size(); i++) {
+            System.out.println(nomes1.get(i));
         }
+
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        System.out.println(integers);
     }
 }
