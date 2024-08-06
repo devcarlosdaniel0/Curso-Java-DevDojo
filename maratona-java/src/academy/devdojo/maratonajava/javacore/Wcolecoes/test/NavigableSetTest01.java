d8ed3ea6b42800798cb019b3fa6f3ecf1a820830
Carlos <cadanielmarchesin@gmail.com>
2024-08-05 22:02:54 -0300

    NavigableSet, TreeSet pt 02
    Métodos interessantes
    descendingSet() - Organiza do maior para o menor
    lower - <
    floor - <=
    higher - >
    ceiling - >=
    poolFirst() - Remove o primeiro elemento da lista
    pollLast() - Remove o último elemento da lista

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Wcolecoes/test/NavigableSetTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wcolecoes/test/NavigableSetTest01.java
@@ -21,7 +21,7 @@ public class NavigableSetTest01 {
         set.add(smartphone);
         System.out.println(set);
 
-        NavigableSet<Jogo> jogos = new TreeSet<>();
+        NavigableSet<Jogo> jogos = new TreeSet<>(new JogoByPrecoComparator());
         jogos.add(new Jogo(5L, "Stardew Valley", 19.9D, 0));
         jogos.add(new Jogo(1L, "Red Dead 2", 9.5D, 5));
         jogos.add(new Jogo(4L,"Gta 5", 3.2D, 3));
@@ -32,5 +32,29 @@ public class NavigableSetTest01 {
         for (Jogo jogo : jogos) {
             System.out.println(jogo);
         }
+
+        // Métodos interessantes
+        // descendingSet() - Organiza do maior para o menor
+        // lower - <
+        // floor - <=
+        // higher - >
+        // ceiling - >=
+        // poolFirst() - Remove o primeiro elemento da lista
+        // pollLast() - Remove o último elemento da lista
+
+        System.out.println();
+
+        Jogo mine = new Jogo(21L, "minecraft", 5.75, 5);
+        System.out.println(jogos.lower(mine));
+        System.out.println(jogos.floor(mine));
+        System.out.println(jogos.higher(mine));
+        System.out.println(jogos.ceiling(mine));
+
+        System.out.println();
+
+        System.out.println(jogos.size());
+        System.out.println(jogos.pollFirst());
+        System.out.println(jogos.pollLast());
+        System.out.println(jogos.size());
     }
 }
