4db5e45344acc5d281ca6c587c9862706007c305
Carlos <cadanielmarchesin@gmail.com>
2024-08-08 15:39:00 -0300

    Wildcard pt 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Xgenerics/test/WildcardTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Xgenerics/test/WildcardTest02.java
@@ -1,16 +1,31 @@
 package academy.devdojo.maratonajava.javacore.Xgenerics.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class WildcardTest02 {
     public static void main(String[] args) {
-        List<Cachorro> cachorroList = List.of(new Cachorro(), new Cachorro());
-        List<Gato> gatoList = List.of(new Gato(), new Gato());
+        List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
+        List<Gato> gatos = List.of(new Gato(), new Gato());
+        printConsulta(cachorros);
+        printConsulta(gatos);
+
+        List<Animal> animals = new ArrayList<>();
+        printConsultaAnimal(animals);
+
+        // extends - são os filhos de animais
+        // super - são necessariamente um animal, ou algo acima de um animal
     }
 
-    private static void printConsulta(Animal[] animals) {
+    private static void printConsulta(List<? extends Animal> animals) {
         for (Animal animal : animals) {
             animal.consulta();
         }
+    }
+
+    private static void printConsultaAnimal(List<? super Animal> animals) {
+        animals.add(new Cachorro());
+        animals.add(new Gato());
     }
 }
