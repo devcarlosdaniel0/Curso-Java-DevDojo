745ff4bf6378e770ddc7f4234138acd57b40ba56
Carlos <cadanielmarchesin@gmail.com>
2024-08-08 19:09:20 -0300

    Métodos Genéricos

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Xgenerics/test/MetodoGenericoTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Xgenerics/test/MetodoGenericoTest01.java
@@ -1,7 +1,20 @@
 package academy.devdojo.maratonajava.javacore.Xgenerics.test;
 
+import academy.devdojo.maratonajava.javacore.Xgenerics.dominio.Carro;
+
+import java.util.List;
+
 public class MetodoGenericoTest01 {
     public static void main(String[] args) {
-        
+        List<Carro> carroList = criarArrayComUmObjeto(new Carro("Lamborghini"));
+        System.out.println(carroList);
     }
+
+    private static <T> List<T> criarArrayComUmObjeto(T t) {
+        return List.of(t);
+    }
+
+//    private static <T extends Comparable<T>> List<T> criarArrayComUmObjeto(T t) {
+//        return List.of(t);
+//    }
 }
