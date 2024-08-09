dc01d096420265830bd5253b2253c29d344a06a0
Carlos <cadanielmarchesin@gmail.com>
2024-08-09 14:54:40 -0300

    lasses Internas pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Yclassesinternas/test/OuterClassTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Yclassesinternas/test/OuterClassTest01.java
@@ -1,7 +1,21 @@
 package academy.devdojo.maratonajava.javacore.Yclassesinternas.test;
 
 public class OuterClassTest01 {
-    public static void main(String[] args) {
+    private String name = "Giorno Giovanna";
 
+    class Inner {
+        private String name = "Luffy";
+        public void printOuterClassAttributes() {
+            System.out.println(name);
+            System.out.println(OuterClassTest01.this.name);
+        }
+    }
+
+    public static void main(String[] args) {
+//        OuterClassTest01 outerClass = new OuterClassTest01();
+//        OuterClassTest01.Inner inner2 = outerClass.new Inner();
+//        inner2.printOuterClassAttributes();
+        OuterClassTest01.Inner inner = new OuterClassTest01().new Inner();
+        inner.printOuterClassAttributes();
     }
 }
