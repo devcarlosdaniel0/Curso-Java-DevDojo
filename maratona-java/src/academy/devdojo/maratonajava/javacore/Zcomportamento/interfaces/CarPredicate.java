d89d05935adfcad18dc870c6c0c165d9dc23bbbf
Carlos <cadanielmarchesin@gmail.com>
2024-08-10 09:44:28 -0300

    Update CarPredicate.java

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Zcomportamento/interfaces/CarPredicate.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zcomportamento/interfaces/CarPredicate.java
@@ -2,6 +2,10 @@ package academy.devdojo.maratonajava.javacore.Zcomportamento.interfaces;
 
 import academy.devdojo.maratonajava.javacore.Zcomportamento.dominio.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
+    // anonimas, funções, conciso
+    // (parametro) -> <expressão>
+    // (Car car) -> car.getColor().equals("green");
     boolean test(Car car);
 }
