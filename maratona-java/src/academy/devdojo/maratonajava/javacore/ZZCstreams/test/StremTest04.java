24e2c21ce731c6e3df82e1e3e2a9bb5b9043f8e5
Carlos <cadanielmarchesin@gmail.com>
2024-08-14 20:14:50 -0300

    Streams pt 04 - FlatMap pt 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZCstreams/test/StremTest04.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.ZZCstreams.test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StremTest04 {
+    public static void main(String[] args) {
+        List<List<String>> escola = new ArrayList<>();
+        List<String> estudantes = List.of("Cazé", "Nicolas", "Bacchin");
+        List<String> professores = List.of("Sandra", "Dani", "Tchesko");
+        List<String> funcionarios = List.of("Tia", "Fabiana");
+        escola.add(estudantes);
+        escola.add(professores);
+        escola.add(funcionarios);
+
+        for (List<String> pessoas : escola) {
+            for (String pessoa : pessoas) {
+                System.out.println(pessoa);
+            }
+        }
+        System.out.println("--------");
+
+        Stream<String> stringStream = escola.stream()
+                .flatMap(Collection::stream);
+        stringStream.forEach(System.out::println);
+    }
+}
