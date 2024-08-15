5876b6c0d926e22483f2eec8d1915913de9ed943
Carlos <cadanielmarchesin@gmail.com>
2024-08-14 21:52:18 -0300

    Streams pt 07 - Reduce pt 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZCstreams/test/ReduceTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.ZZCstreams.test;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ReduceTest01 {
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+        integers.stream().reduce((x, y) -> x + y).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(0, (x, y) -> x + y));
+
+        integers.stream().reduce(Integer::sum).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(0, Integer::sum));
+
+        integers.stream().reduce((x, y) -> x * y).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(1, (x, y) -> x * y));
+    }
+}
