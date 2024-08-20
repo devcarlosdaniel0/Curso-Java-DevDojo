e05256ffa15da12dc213b6c90129d3077f3e24e1
Carlos <cadanielmarchesin@gmail.com>
2024-08-20 15:23:24 -0300

    Parallel Streams pt 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZCstreams/test/ParallelStreamsTest01.java
@@ -0,0 +1,59 @@
+package academy.devdojo.maratonajava.javacore.ZZCstreams.test;
+
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+public class ParallelStreamsTest01 {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        long num = 100_000_000;
+        sumFor(num);
+        sumStreamIterate(num);
+        sumParallelStreamIterate(num);
+        sumLongStreamIterate(num);
+        sumParallelLongStreamIterate(num);
+    }
+
+    private static void sumFor(long num) {
+        System.out.println("Sum for");
+        long result = 0;
+        long init = System.currentTimeMillis();
+        for (long i = 1; i <= num; i++) {
+            result += i;
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(result + " " + (end - init) + "ms");
+    }
+
+    private static void sumStreamIterate(long num) {
+        System.out.println("Sum streamIterate");
+        long init = System.currentTimeMillis();
+        long result = Stream.iterate(1L, i -> i + 1).limit(num).reduce(0L, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println(result + " " + (end - init) + "ms");
+    }
+
+    private static void sumParallelStreamIterate(long num) {
+        System.out.println("Sum ParallelStreamIterate");
+        long init = System.currentTimeMillis();
+        long result = Stream.iterate(1L, i -> i + 1).limit(num).parallel().reduce(0L, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println(result + " " + (end - init) + "ms");
+    }
+
+    private static void sumLongStreamIterate(long num) {
+        System.out.println("Sum LongStreamIterate");
+        long init = System.currentTimeMillis();
+        Long result = LongStream.rangeClosed(1L, num).reduce(1L, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println(result + " " + (end - init) + "ms");
+    }
+
+    private static void sumParallelLongStreamIterate(long num) {
+        System.out.println("Sum ParallelLongStreamIterate");
+        long init = System.currentTimeMillis();
+        Long result = LongStream.rangeClosed(1L, num).parallel().reduce(1L, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println(result + " " + (end - init) + "ms");
+    }
+}
