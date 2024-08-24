9de0c6bb668aff792c901223de3d679621be3981
Carlos <cadanielmarchesin@gmail.com>
2024-08-24 10:16:46 -0300

    Sincronismo de thread pt 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZDthreads/test/ThreadAccountTest01.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.ZZDthreads.test;
+
+import academy.devdojo.maratonajava.javacore.ZZDthreads.dominio.Account;
+
+public class ThreadAccountTest01 implements Runnable {
+    private Account account = new Account();
+    public static void main(String[] args) {
+        ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
+        Thread t1 = new Thread(threadAccountTest01, "T1");
+        Thread t2 = new Thread(threadAccountTest01, "T2");
+        t1.start();
+        t2.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            withdrawal(10);
+            if (account.getBalance() < 0) {
+                System.out.println("FUDEU");
+            }
+        }
+    }
+
+    private void withdrawal(int amount) {
+        if (account.getBalance() >= amount) {
+            System.out.println(getThreadName() + " está indo sacar dinheiro");
+            account.withdrawal(amount);
+            System.out.println(getThreadName() + " completou o saque, valor atual " + account.getBalance());
+        } else {
+            System.out.println("Não tem dinheiro para " + getThreadName() + " efetuar o saque " + account.getBalance());
+        }
+    }
+
+    private static String getThreadName() {
+        return Thread.currentThread().getName();
+    }
+}
