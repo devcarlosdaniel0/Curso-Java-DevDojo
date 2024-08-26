35421cc4f60a905f8c4f1b664a3cea94c8192ca9
Carlos <cadanielmarchesin@gmail.com>
2024-08-26 11:14:21 -0300

    Classes thread safe

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZDthreads/test/ThreadSafeTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZDthreads/test/ThreadSafeTest01.java
@@ -1,7 +1,38 @@
 package academy.devdojo.maratonajava.javacore.ZZDthreads.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class ThreadSafeNames {
+    private final List<String> names = new ArrayList<>();
+
+    public synchronized void add(String name) {
+        names.add(name);
+    }
+
+    public synchronized void removeFirst() {
+        if (names.size() > 0) {
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(names.remove(0));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ThreadSafeNames{" +
+                "names=" + names +
+                '}';
+    }
+}
+
 public class ThreadSafeTest01 {
     public static void main(String[] args) {
-        
+        ThreadSafeNames threadSafeNames = new ThreadSafeNames();
+        threadSafeNames.add("cleber");
+        Runnable r = threadSafeNames::removeFirst;
+        new Thread(r).start();
+        new Thread(r).start();
+
+        System.out.println(threadSafeNames);
     }
 }
