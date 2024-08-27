10743d1b378eb463d8788d93d38b1cc81e8a7dcf
Carlos <cadanielmarchesin@gmail.com>
2024-08-27 11:49:24 -0300

    Lock and ReentrantLock

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEconcorrencia/test/AtomicIntegerTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEconcorrencia/test/AtomicIntegerTest01.java
@@ -1,18 +1,35 @@
 package academy.devdojo.maratonajava.javacore.ZZEconcorrencia.test;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Counter {
     private int count;
+    private int count2;
     private AtomicInteger atomicInteger = new AtomicInteger();
+    private Lock lock = new ReentrantLock(true);
 
     public void increment() {
-        count++;
-        atomicInteger.incrementAndGet();
+        lock.lock();
+        try {
+            count++;
+            atomicInteger.incrementAndGet();
+        } finally {
+            lock.unlock();
+        }
+
+        synchronized (this) {
+            count2++;
+        }
     }
 
     public int getCount() {
         return count;
+    }
+
+    public int getCount2() {
+        return count2;
     }
 
     public AtomicInteger getAtomicInteger() {
@@ -36,5 +53,6 @@ public class AtomicIntegerTest01 {
         t2.join();
         System.out.println(counter.getCount());
         System.out.println(counter.getAtomicInteger());
+        System.out.println(counter.getCount2());
     }
 }
