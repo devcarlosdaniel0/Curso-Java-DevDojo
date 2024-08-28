f88dea311bcd5d07b697bc3294b922757fccc339
Carlos <cadanielmarchesin@gmail.com>
2024-08-28 19:13:34 -0300

    ReentrantReadWriteLock

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEconcorrencia/test/ReentrantReadWriteLockTest01.java
@@ -0,0 +1,67 @@
+package academy.devdojo.maratonajava.javacore.ZZEconcorrencia.test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+class MapReadWrite {
+    private final Map<String, String> map = new LinkedHashMap<>();
+    private final ReentrantReadWriteLock rwl;
+
+    MapReadWrite(ReentrantReadWriteLock rwl) {
+        this.rwl = rwl;
+    }
+
+    public void put(String key, String value) {
+        rwl.writeLock().lock();
+        try {
+            if(rwl.isWriteLocked()) {
+                System.out.printf("%s pegou o WRITE LOCK%n", Thread.currentThread().getName());
+            }
+            map.put(key, value);
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            rwl.writeLock().unlock();
+        }
+    }
+
+    public Set<String> allkeys () {
+        rwl.readLock().lock();
+        try {
+            return map.keySet();
+        } finally {
+            rwl.readLock().unlock();
+        }
+    }
+}
+
+public class ReentrantReadWriteLockTest01 {
+    public static void main(String[] args) {
+        ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+        MapReadWrite mapReadWrite = new MapReadWrite(rwl);
+
+        Runnable writer = () -> {
+            for (int i = 0; i < 20; i++) {
+                mapReadWrite.put(String.valueOf(i), String.valueOf(i));
+            }
+        };
+
+        Runnable reader = () -> {
+            if (rwl.isWriteLocked()) {
+                System.out.println("WRITE LOCKED!");
+            }
+            System.out.println("I got the lock!");
+            System.out.println(Thread.currentThread().getName() + " " + mapReadWrite.allkeys());
+        };
+
+        Thread t1 = new Thread(writer);
+        Thread t2 = new Thread(reader);
+        Thread t3 = new Thread(reader);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
