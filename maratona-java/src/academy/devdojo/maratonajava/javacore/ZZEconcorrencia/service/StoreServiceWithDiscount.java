259c56a89d5ee611a45fa882cba83f038955ebc3
Carlos <cadanielmarchesin@gmail.com>
2024-09-03 10:25:20 -0300

    CompletableFuture pt 04 - Encadeando chamadas pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEconcorrencia/service/StoreServiceWithDiscount.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEconcorrencia/service/StoreServiceWithDiscount.java
@@ -1,5 +1,8 @@
 package academy.devdojo.maratonajava.javacore.ZZEconcorrencia.service;
 
+import academy.devdojo.maratonajava.javacore.ZZEconcorrencia.dominio.Discount;
+import academy.devdojo.maratonajava.javacore.ZZEconcorrencia.dominio.Quote;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
@@ -8,8 +11,17 @@ import java.util.concurrent.TimeUnit;
 public class StoreServiceWithDiscount {
     private static final ExecutorService ex = Executors.newCachedThreadPool();
 
-    public double getPriceSync(String storeName) {
-        return priceGenerator();
+    public String getPriceSync(String storeName) {
+        double price = priceGenerator();
+        Discount.Code discountCode = Discount.Code.values()[ThreadLocalRandom.current().nextInt(Discount.Code.values().length)];
+        return String.format("%s:%.2f:%s", storeName, price, discountCode);
+    }
+
+    public String applyDiscount(Quote quote) {
+        delay();
+        double discountValue = quote.getPrice() * (100 - quote.getDiscountCode().getPercentage()) / 100;
+        return String.format("'%s' original price: '%.2f'. Applying Discount code '%s'. Final price: '%.2f'",
+                quote.getStore(), quote.getPrice(), quote.getDiscountCode(), discountValue);
     }
 
     private double priceGenerator() {
