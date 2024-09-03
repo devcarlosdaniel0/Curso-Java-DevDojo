54cef1d47d259848093bc840b2c6f2c87c62c96b
Carlos <cadanielmarchesin@gmail.com>
2024-09-03 10:44:48 -0300

    CompletableFuture pt 05 - Encadeando chamadas pt 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEconcorrencia/test/CompletableFutureTest04.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEconcorrencia/test/CompletableFutureTest04.java
@@ -4,20 +4,44 @@ import academy.devdojo.maratonajava.javacore.ZZEconcorrencia.dominio.Quote;
 import academy.devdojo.maratonajava.javacore.ZZEconcorrencia.service.StoreServiceWithDiscount;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class CompletableFutureTest04 {
     public static void main(String[] args) {
         StoreServiceWithDiscount service = new StoreServiceWithDiscount();
-        searchPricesWithDiscount(service);
+        searchPricesWithDiscountAsync(service);
     }
 
-    private static void searchPricesWithDiscount(StoreServiceWithDiscount service) {
+    private static void searchPricesWithDiscountSync(StoreServiceWithDiscount service) {
         List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+        long start = System.currentTimeMillis();
 //        stores.forEach(s -> System.out.println(service.getPriceSync(s)));
         stores.stream()
                 .map(service::getPriceSync)
                 .map(Quote::newQuote)
                 .map(service::applyDiscount)
                 .forEach(System.out::println);
+
+        long end = System.currentTimeMillis();
+        System.out.printf("Time passed to searchPricesSync: %dms%n", (end - start));
+    }
+
+    private static void searchPricesWithDiscountAsync(StoreServiceWithDiscount service) {
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+        long start = System.currentTimeMillis();
+
+        List<CompletableFuture<String>> completableFutures = stores.stream()
+                .map(s -> CompletableFuture.supplyAsync(() -> service.getPriceSync(s)))
+                .map(cf -> cf.thenApply(Quote::newQuote))
+                .map(cf -> cf.thenCompose(quote -> CompletableFuture.supplyAsync(() -> service.applyDiscount(quote))))
+                .collect(Collectors.toList());
+
+        completableFutures.stream()
+                .map(CompletableFuture::join)
+                .forEach(System.out::println);
+
+        long end = System.currentTimeMillis();
+        System.out.printf("Time passed to searchPricesSync: %dms%n", (end - start));
     }
 }
