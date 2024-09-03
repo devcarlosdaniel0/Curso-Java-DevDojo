2e97fe3bbbb89a98e45f37a661e48076eefdb2a2
Carlos <cadanielmarchesin@gmail.com>
2024-09-03 11:02:30 -0300

    CompletableFuture pt 06 - allOf, anyOf

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEconcorrencia/service/StoreServiceWithDiscount.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEconcorrencia/service/StoreServiceWithDiscount.java
@@ -31,7 +31,8 @@ public class StoreServiceWithDiscount {
 
     private void delay() {
         try {
-            TimeUnit.SECONDS.sleep(1);
+            int milli = ThreadLocalRandom.current().nextInt(200, 2500);
+            TimeUnit.MICROSECONDS.sleep(milli);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
