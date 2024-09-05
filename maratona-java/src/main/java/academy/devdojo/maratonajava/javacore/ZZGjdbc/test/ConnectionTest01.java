eb87650067b47c9076077606764bedb3bef2d712
Carlos <cadanielmarchesin@gmail.com>
2024-09-05 16:03:44 -0300

    Inserindo dados com Statement

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/test/ConnectionTest01.java
+++
@@ -1,9 +0,0 @@
-package academy.devdojo.maratonajava.javacore.ZZGjdbc.test;
-
-import academy.devdojo.maratonajava.javacore.ZZGjdbc.conn.ConnectionFactory;
-
-public class ConnectionTest01 {
-    public static void main(String[] args) {
-        ConnectionFactory.getConnection();
-    }
-}
