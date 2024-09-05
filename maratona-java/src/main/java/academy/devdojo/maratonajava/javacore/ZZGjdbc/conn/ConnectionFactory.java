0e334b4a68940a61260a8ae609dda1d7e4591e4b
Carlos <cadanielmarchesin@gmail.com>
2024-09-05 15:34:46 -0300

    JDBC pt 05 - Adicionando dependência e conectando com banco

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/conn/ConnectionFactory.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/conn/ConnectionFactory.java
@@ -1,4 +1,20 @@
 package academy.devdojo.maratonajava.javacore.ZZGjdbc.conn;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+    public static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/anime_store";
+        String username = "root";
+        String password = "root";
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/test/ConnectionTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.ZZGjdbc.test;
+
+import academy.devdojo.maratonajava.javacore.ZZGjdbc.conn.ConnectionFactory;
+
+public class ConnectionTest01 {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
