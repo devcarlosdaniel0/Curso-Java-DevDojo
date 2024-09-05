06c93608da364ac1008533dab8aeeb8c354e43bb
Carlos <cadanielmarchesin@gmail.com>
2024-09-05 16:43:37 -0300

    Lombok e Log4J2

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/test/ConnectionFactoryTest01.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/test/ConnectionFactoryTest01.java
@@ -1,12 +1,11 @@
 package academy.devdojo.maratonajava.javacore.ZZGjdbc.test;
 
-import academy.devdojo.maratonajava.javacore.ZZGjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZGjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZGjdbc.repository.ProducerRepository;
 
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("Bandai").build();
+        Producer producer = Producer.builder().name("Studio Deen").build();
         ProducerRepository.save(producer);
     }
 }
