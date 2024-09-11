dfb98381c7f1106e2dd6139aca88d94ff3c8dc98
Carlos <cadanielmarchesin@gmail.com>
2024-09-11 19:39:50 -0300

    Connected RowSet - JdbcRowSet pt 01

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/service/ProducerServiceRowSet.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.ZZGjdbc.service;
+
+import academy.devdojo.maratonajava.javacore.ZZGjdbc.dominio.Producer;
+import academy.devdojo.maratonajava.javacore.ZZGjdbc.repository.ProducerRepositoryRowSet;
+
+import java.util.List;
+
+public class ProducerServiceRowSet {
+    public static List<Producer> findByNameJdbcRowSet(String name) {
+        return ProducerRepositoryRowSet.findByNameJdbcRowSet(name);
+    }
+}
