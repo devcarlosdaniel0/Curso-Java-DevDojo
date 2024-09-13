d894c8dd52842fb26ff0a145e783b56e87b1878a
Carlos <cadanielmarchesin@gmail.com>
2024-09-13 12:37:40 -0300

    CRUD pt 02 - delete

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZHcrud/repository/ProducerRepository.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZHcrud/repository/ProducerRepository.java
@@ -32,7 +32,24 @@ public class ProducerRepository {
     private static PreparedStatement createPreparedStatmentFindByName(Connection conn, String name) throws SQLException {
         String sql = "SELECT * FROM anime_store.producer where name like ?;";
         PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setString(1, String.format("%%%s%%",name));
+        ps.setString(1, String.format("%%%s%%", name));
+        return ps;
+    }
+
+    public static void delete(int id) {
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPreparedStatmentDelete(conn, id)) {
+            ps.execute();
+            log.info("Deleted producer '{}' from database", id);
+        } catch (SQLException e) {
+            log.error("Error while trying to delete producer '{}'", id, e);
+        }
+    }
+
+    private static PreparedStatement createPreparedStatmentDelete(Connection conn, Integer id) throws SQLException {
+        String sql = "DELETE FROM `anime_store`.`producer` WHERE (`id` = ?);";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, id);
         return ps;
     }
 }
