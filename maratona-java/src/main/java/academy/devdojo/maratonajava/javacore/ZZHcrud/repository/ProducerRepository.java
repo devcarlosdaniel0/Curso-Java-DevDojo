08da54b7b794f7b427aaa6aedb9bf84f4ae82769
Carlos <cadanielmarchesin@gmail.com>
2024-09-13 13:15:24 -0300

    CRUD pt 03 - save

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZHcrud/repository/ProducerRepository.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZHcrud/repository/ProducerRepository.java
@@ -4,10 +4,7 @@ import academy.devdojo.maratonajava.javacore.ZZHcrud.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZHcrud.dominio.Producer;
 import lombok.extern.log4j.Log4j2;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +14,7 @@ public class ProducerRepository {
         log.info("Finding Producer by name '{}'", name);
         List<Producer> producers = new ArrayList<>();
         try (Connection conn = ConnectionFactory.getConnection();
-             PreparedStatement ps = createPreparedStatmentFindByName(conn, name);
+             PreparedStatement ps = createPreparedStatementFindByName(conn, name);
              ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
                 Producer producer = Producer.builder().id(rs.getInt("id")).name(rs.getString("name")).build();
@@ -29,7 +26,7 @@ public class ProducerRepository {
         return producers;
     }
 
-    private static PreparedStatement createPreparedStatmentFindByName(Connection conn, String name) throws SQLException {
+    private static PreparedStatement createPreparedStatementFindByName(Connection conn, String name) throws SQLException {
         String sql = "SELECT * FROM anime_store.producer where name like ?;";
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setString(1, String.format("%%%s%%", name));
@@ -38,7 +35,7 @@ public class ProducerRepository {
 
     public static void delete(int id) {
         try (Connection conn = ConnectionFactory.getConnection();
-             PreparedStatement ps = createPreparedStatmentDelete(conn, id)) {
+             PreparedStatement ps = createPreparedStatementDelete(conn, id)) {
             ps.execute();
             log.info("Deleted producer '{}' from database", id);
         } catch (SQLException e) {
@@ -46,10 +43,29 @@ public class ProducerRepository {
         }
     }
 
-    private static PreparedStatement createPreparedStatmentDelete(Connection conn, Integer id) throws SQLException {
+    private static PreparedStatement createPreparedStatementDelete(Connection conn, Integer id) throws SQLException {
         String sql = "DELETE FROM `anime_store`.`producer` WHERE (`id` = ?);";
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setInt(1, id);
         return ps;
     }
+
+    public static void insert(Producer producer) {
+        log.info("Inserting Producer '{}'", producer);
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPreparedStatementInsert(conn, producer)) {
+            ps.execute();
+        } catch (SQLException e) {
+            log.error("Error while trying to update producer '{}'", producer.getId(), e);
+        }
+    }
+
+    private static PreparedStatement createPreparedStatementInsert(Connection conn, Producer producer) throws SQLException {
+        String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES (?);";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, producer.getName());
+        return ps;
+    }
+
+
 }
